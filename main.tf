48fe6e845ba5a9a846443a10f38503e684df20db
Josue-lab <58716020+Josue-lab@users.noreply.github.com>
2020-06-22 16:02:17 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@ provider "azurerm" {
   version = "=2.5.0"
   features {}
 }
+
+resource "azurerm_app_service_plan" "svcplan" {
+  name                = "servplanvg"
+  location            = "southcentralus"
+  resource_group_name = "RG-Academia-CoE"
+  sku {
+        tier = "Standard"
+        size = "S1"
+  }
+}
+
